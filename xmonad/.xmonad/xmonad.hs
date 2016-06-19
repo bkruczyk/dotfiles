d0944fec1701ba95fd22573467df0f6357064cd2
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2016-06-19 22:21:33 +0200

    snapshot

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,20 +1,25 @@
 import XMonad
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Spacing
+import XMonad.Util.EZConfig ( additionalKeys )
 
-myLayoutHook = avoidStruts $ layoutHook defaultConfig
+myLayoutHook = spacing 4 $ avoidStruts $ layoutHook defaultConfig
 myManageHook = manageDocks <+> manageHook defaultConfig
+myModMask = mod4Mask
+myKeys = [ ((myModMask , xK_r), spawn "rofi -show run")
+         , ((myModMask , xK_e), spawn "rofi -show window")
+         ]
 
 main :: IO()
 main = do
   xmonad $ ewmh defaultConfig
     { terminal           = "termite"
     , focusFollowsMouse  = True
-    , modMask            = mod4Mask
+    , modMask            = myModMask
     , borderWidth        = 4
     , layoutHook         = myLayoutHook
     , manageHook         = myManageHook
-    -- , handleEventHook    = fullscreenEventHook
-    , focusedBorderColor = "#0a9dff" -- tardis
-    , normalBorderColor  = "#d9cec3" -- brightgravel
-    }
+    , focusedBorderColor = "#0a9dff"
+    , normalBorderColor  = "#d9cec3"
+    } `additionalKeys` myKeys
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,14 +1,14 @@
 sudo loadkeys ~/.loadkeys
 
 PANEL_FIFO=/tmp/panel-fifo
-PANEL_FONT="-lucy-tewi-medium-r-normal--*-*-*-*-*-*-iso10646-1"
-# PANEL_FONT="-misc-fixed-medium-r-semicondensed-*-*-120-*-*-*-*-*-*"
-PANEL_HEIGHT=24
-PANEL_WM_NAME=bspwm_panel
-export PANEL_FIFO PANEL_HEIGHT PANEL_FONT PANEL_WM_NAME
+PANEL_FONT="Monaco:size=9"
+PANEL_HEIGHT=40
+PANEL_WM_NAME=lemonbar_panel
 
+export PANEL_FIFO PANEL_HEIGHT PANEL_FONT PANEL_WM_NAME
 export _JAVA_AWT_WM_NONREPARENTING=1
 
+# start gnome-keyring daemon
 if [ -z $SSH_AUTH_SOCK ]; then
     eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
     export SSH_AUTH_SOCK
